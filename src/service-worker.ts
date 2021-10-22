77a1676afde2d2d46520fd8439a94b66d0cc685c
wicho1001 <wichoguerrero747@gmail.com>
2021-10-22 10:10:57 -0500

    Initial commit :rocket:

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -31,6 +31,7 @@ const fileExtensionRegexp = new RegExp('/[^/?]+\\.[^/]+$');
 registerRoute(
   // Return false to exempt requests from being fulfilled by index.html.
   ({ request, url }: { request: Request; url: URL }) => {
+    console.log('hello')
     // If this isn't a navigation, skip.
     if (request.mode !== 'navigate') {
       return false;
